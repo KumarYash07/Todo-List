dbfe0671b3e08e4f89bcabdc77b21f811fe26221
KumarYash07 <yash.23b0101151@abes.ac.in>
2026-06-14 12:49:53 +0530

    Add My Name

--- src/componenets/AppName.jsx
+++ src/componenets/AppName.jsx
@@ -1,6 +1,9 @@
 import styles from "./AppName.module.css"
 function AppName(){
-    return <h1 className={styles.todoHeading}>TODO List </h1>;
+    return<>
+     <h1 className={styles.todoHeading}>TODO List </h1>
+        <p>Yash Kumar</p>
+    </>
 }
 
 export default AppName
